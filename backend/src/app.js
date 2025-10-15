44ca4172b49be432bee9ea1fc1991a2772a48f72
Muhammad Tayyab <muhammadtayyab742002@gmail.com>
2025-10-15 16:52:09 +0500

    add signup and signin routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+require("dotenv").config();
 
-const rootRouter = require('./routes/root');
+const rootRouter = require("./routes/root");
+const authRouter = require("./routes/auth");
 
 const app = express();
 
 app.use(helmet());
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use('/', rootRouter);
+app.use("/", rootRouter);
+app.use("/auth", authRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ error: 'Not Found' });
+  res.status(404).json({ error: "Not Found" });
 });
 
 app.use((err, req, res, next) => {
   // Basic error handler
   // eslint-disable-next-line no-console
   console.error(err);
-  res.status(500).json({ error: 'Internal Server Error' });
+  res.status(500).json({ error: "Internal Server Error" });
 });
 
 module.exports = app;
-
-
